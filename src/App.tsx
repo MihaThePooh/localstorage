7595cdac62822b1e459df48a796953bc171729d8
Miha <mihailperov90@gmail.com>
2021-03-26 00:26:09 +0300

    localstorage 1 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState(0);
+
+  const incHandler = () => {
+    setValue(value + 1)
+  };
+
+  const setToLocalStorageHandler = () => {
+    localStorage.setItem('counterValue', JSON.stringify(value));
+    localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+  };
+
+  const getFromLocalStorageHandler = () => {
+    let valueAsString = localStorage.getItem('counterValue')
+    if (valueAsString) {
+      let newValue = JSON.parse(valueAsString);
+      setValue(newValue)
+    }
+  };
+
+  const clearLocalStorageHandler = () => {
+    localStorage.clear();
+    setValue(0)
+  };
+
+  const removeFromLocalStorageHandler = () => {
+    localStorage.removeItem('counterValue + 1')
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{value}</h1>
+        <div><button onClick={incHandler}>Inc</button></div>
+        <div><button onClick={setToLocalStorageHandler}>setToLocalStorageHandler</button></div>
+        <div><button onClick={getFromLocalStorageHandler}>getFromLocalStorageHandler</button></div>
+        <div><button onClick={clearLocalStorageHandler}>clearLocalStorageHandler</button></div>
+        <div><button onClick={removeFromLocalStorageHandler}>removeFromLocalStorageHandler</button></div>
       </header>
     </div>
   );
