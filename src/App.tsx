bdc5c1240a34c2aa70ca57985756fd57a2c058e5
Miha <mihailperov90@gmail.com>
2021-03-26 00:49:41 +0300

    add UseEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
 
     const [value, setValue] = useState(0);
     const [sessionValue, setSessionValue] = useState(0);
+
+    useEffect( () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString);
+            setValue(newValue)
+        }
+    }, [] );
+
+    useEffect( () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }, [value] );
 
     const incHandler = () => {
         setValue(value + 1);
